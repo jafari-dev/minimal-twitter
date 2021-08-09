b8958165970c227bb7f5c87f14e3030b2c9abde5
Ahmad Jafari <a99jafari@gmail.com>
2021-08-10 04:00:00 +0430

    Fetch & show posts from json server

--- src/layouts/Posts/index.tsx
+++ src/layouts/Posts/index.tsx
@@ -1,35 +1,24 @@
-import { useState, memo, useCallback } from "react";
+import { useState, memo, useCallback, useEffect } from "react";
 
 import { Post, SendPost } from "../../components";
 import { PostType } from "../../utilities/types";
 import { StyledPosts, StyledContainer } from "./styles";
-import { DefaultAvatar, Heart } from "../../images"; 
-
-const initialPosts: PostType[] = [
-    {
-        id: "POST-1",
-        date: "2021/05/31",
-        likes: 76,
-        ownerAvatar: DefaultAvatar,
-        ownerFullname: "Robert Deniro",
-        ownerId: "deniro",
-        text: "This a simple test for Post component"
-    },
-    {
-        id: "POST-2",
-        date: "2018/11/17",
-        likes: 5783,
-        ownerAvatar: DefaultAvatar,
-        ownerFullname: "Natalie Portman",
-        ownerId: "natalie",
-        text: "This a simple test for Post component again!",
-        imageAttachment: Heart
-    },
-]
 
 
 function PostsComponent(): React.ReactElement {
-    const [posts, setPosts] = useState<PostType[]>(initialPosts);
+    const [posts, setPosts] = useState<PostType[]>([]);
+
+    useEffect(() => {
+        (async function() {
+            try {
+                const response = await fetch("http://localhost:4000/posts");
+                const posts = await response.json();
+                setPosts(posts);
+            } catch(error) {
+                console.log(error);
+            }
+        })();
+    }, []);
 
     const handleSendNewPost = useCallback((newPost: PostType) => {
         setPosts([newPost, ...posts])
